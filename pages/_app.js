e405680fe8c64115fac3cbac49ff83604abca502
markmead <itsmarkmead@gmail.com>
2022-04-03 17:19:12 +0100

    Add SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,44 @@
 import "../styles/globals.css";
 
+import Head from "next/head";
+
 import { Layout } from "../components/Layout";
 import { Header } from "../components/Header";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Header />
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-4HQZYDVZ32"
+      ></script>
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-4HQZYDVZ32');
+          `,
+        }}
+      />
+
+      <Head>
+        <title>Tailwind CSS Wordle | HyperUI</title>
+        <meta
+          name="description"
+          content="Guess from over 6000 Tailwind CSS class names. It's Wordle for Tailwind CSS."
+          key="description"
+        />
+      </Head>
+
+      <Layout>
+        <Header />
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
