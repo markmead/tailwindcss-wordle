af3001afd894475cfc830de150b3a0493aea3ea7
markmead <itsmarkmead@gmail.com>
2022-04-05 09:13:39 +0100

    Fix code

--- components/GridGuesses.js
+++ components/GridGuesses.js
@@ -1,18 +1,19 @@
-import { useState, useEffect } from "react";
-
-import { GuessCard } from "./GuessCard";
-
 export const GridGuesses = ({ guesses }) => (
   <div className="space-y-1">
     {guesses.map((guess, index) => (
       <ul className="flex gap-1 sm:justify-center" key={index}>
         {guess.map(({ letter, correct, included }, index) => (
-          <GuessCard
+          <li
             key={index}
-            letter={letter}
-            correct={correct}
-            included={included}
-          />
+            className={`
+              w-10 h-10 leading-10 shrink-0 text-center text-sm font-medium bg-gray-700 rounded-sm
+              ${correct && "bg-green-600 text-white"}
+              ${!correct && included && "bg-yellow-600 text-white"}
+              ${!correct && !included && "bg-red-600 text-white"}
+            `}
+          >
+            {letter}
+          </li>
         ))}
       </ul>
     ))}
